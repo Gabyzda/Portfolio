0dcd584675876912488eea415d63b3c3ad594579
Gaby Zapata <gabyzda@gmail.com>
2022-07-15 23:33:47 -0300

    funcion constructora de  datos usuario

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let usuarios = []; //array que guarda los objetos de personas
+
+function Usuario(nome,sobrenome,email){ // função construtora para recaudar los datos de cada usuario
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.email = email
+}

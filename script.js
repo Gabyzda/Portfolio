bec814f78521bec161c0af603e16cddb58b18ec0
Gaby Zapata <gabyzda@gmail.com>
2022-07-15 23:59:23 -0300

    Variables para guardar los valores de cada input

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function Usuario(nome,sobrenome,email){ // função construtora para recaudar lo
     this.sobrenome = sobrenome;
     this.email = email
 }
+// Para guardar los valores de cada input
+let nome = document.querySelector('#input_nome')
+let idade = document.querySelector('#input_idade')
+let email = document.querySelector('#input_email')

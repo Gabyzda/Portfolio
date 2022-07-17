b6929daa3c3f22a5a9c4e34f10c5a6a28e6ead4a
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 10:29:21 -0300

    linkada función con botón y acción  limpiar inputs

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
  //array que guarda los objetos de personas
-let usuarios = [];
+ let usuarios = [];
 
-// función construtora para recaudar los datos de cada usuario
-function Usuario(nome,sobrenome,email){ 
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    this.email = email
-}
-// Para guardar los valores de cada input
-let nome = document.querySelector('#input_nome')
-let sobrenome = document.querySelector('#input_sobrenome')
-let email = document.querySelector('#input_email')
+ // función construtora para recaudar los datos de cada usuario
+ function Usuario(nome, sobrenome, email) {
+     this.nome = nome;
+     this.sobrenome = sobrenome;
+     this.email = email
+ }
+ // Para guardar los valores de cada input
+ let nome = document.querySelector('#input_nome')
+ let sobrenome = document.querySelector('#input_sobrenome')
+ let email = document.querySelector('#input_email')
 
-// función para guardar registro
-function AddRegistroNovo(){ 
-    let nomeUsuario = nome.value;
-    let sobrenomeUsuario = sobrenome.value;
-    let emailUsuario = email.value;
-    // Nuevo objeto Usuario para crear conjunto de valores de usuario desde input
-    let usuario = new Usuario(nomeUsuario,sobrenomeUsuario,emailUsuario)
-//  Vamos a crear una lista de los usuarios a través de estos nuevos usuarios
-    usuarios.push(usuario);
-}
+ // función para guardar registro
+ function AddRegistroNovo() {
+     let nomeUsuario = nome.value;
+     let sobrenomeUsuario = sobrenome.value;
+     let emailUsuario = email.value;
+     // Nuevo objeto Usuario para crear conjunto de valores de usuario desde input
+     let usuario = new Usuario(nomeUsuario, sobrenomeUsuario, emailUsuario)
+     //  Vamos a crear una lista de los usuarios a través de este new usuario
+     usuarios.push(usuario);
+    //  limpiar los inputs
+     nome.value = ""
+     sobrenome.value = ""
+     email.value = ""
+ }
+ // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
+ document.querySelector('#btn_submit').addEventListener('click',
+     function (evt) {
+         evt.preventDefault();
+     })

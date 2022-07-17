a77c7eaf818b174107fc380650610993bc3cc60a
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 19:49:38 -0300

    Probando queryselectorall para imput

--- script.js
+++ script.js
@@ -70,3 +70,8 @@
 
  //  Evento para validación de campo
  document.querySelector('#btn_submit').addEventListener('click', ValidacaoCampo)
+
+//   globales... para todos los input
+document.querySelectorAll('input').forEach(el => el.classList.add ('not-validated'));
+document.querySelectorAll('input#email').forEach(el => el.addEventListener ('click', validateEmail));
+document.querySelectorAll('input:required').forEach(el => el.addEventListener ('click', validateNotEmpty));

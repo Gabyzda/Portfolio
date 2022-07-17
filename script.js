0d5c234db30732d7ac1b9a43f7c10b871f0c580c
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 13:02:50 -0300

    cambiar alert por smsError push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
- //array que guarda los objetos de personas
+ // array que guarda los objetos de personas
  let usuarios = [];
-//  array que guarda los mensajes de error
+ //  array que guarda los mensajes de error
  let smsError = [];
 
  // función construtora para recaudar los datos de cada usuario
@@ -29,32 +29,32 @@
          //  sobrenome.value = ""
          //  email.value = ""
          form.reset();
-     }else 
-    }
+     } else
+ }
 
-     //  garantizar que los campos estén correctamente preenchidos, "Validación de datos" y/o que no estén vacios al clicar botón
-     function ValidacaoCampo() {
-         if (nome.value == "") {
-             alert("O campo Nome é obrigatório. Favor preenchê-lo.")
-             nome.focus()
-             return 0
-         } else if (sobrenome.value == "") {
-             alert("O campo Sobrenome é obrigatório. Favor preenchê-lo.")
-             sobrenome.focus()
-             return 0
-         } else if (email.value == "") {
-             alert("O campo Email é obrigatório. Favor preenchê-lo.")
-             email.focus()
-             return 0
-         }
+ //  garantizar que los campos estén correctamente preenchidos, "Validación de datos" y/o que no estén vacios al clicar botón
+ function ValidacaoCampo() {
+     if (nome.value == "") {
+         smsError.push("O campo Nome é obrigatório. Favor preenchê-lo.")
+         nome.focus()
+         return 0
+     } else if (sobrenome.value == "") {
+         smsError.push("O campo Sobrenome é obrigatório. Favor preenchê-lo.")
+         sobrenome.focus()
+         return 0
+     } else if (email.value == "") {
+         smsError.push("O campo Email é obrigatório. Favor preenchê-lo.")
+         email.focus()
+         return 0
      }
+ }
 
-     // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
-     document.querySelector('#btn_submit').addEventListener('click',
-         function (evt) {
-             evt.preventDefault();
-         })
-     document.querySelector('#btn_submit').addEventListener('click', AddRegistroNovo)
+ // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
+ document.querySelector('#btn_submit').addEventListener('click',
+     function (evt) {
+         evt.preventDefault();
+     })
+ document.querySelector('#btn_submit').addEventListener('click', AddRegistroNovo)
 
-     //  Evento para validación de campo
-     document.querySelector('#btn_submit').addEventListener('click', ValidacaoCampo)
+ //  Evento para validación de campo
+ document.querySelector('#btn_submit').addEventListener('click', ValidacaoCampo)

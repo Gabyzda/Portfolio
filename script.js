779438f49444c9f6fbc1ce9dd8f4e403dbf294b5
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 11:43:14 -0300

    Reset inputs y validar campos form con alert

--- script.js
+++ script.js
@@ -21,11 +21,27 @@
      let usuario = new Usuario(nomeUsuario, sobrenomeUsuario, emailUsuario)
      //  Vamos a crear una lista de los usuarios a través de este new usuario
      usuarios.push(usuario);
-    //  limpiar los inputs
-     nome.value = ""
-     sobrenome.value = ""
-     email.value = ""
+     //  limpiar los inputs, no estoy segura...
+     //  nome.value = ""
+     //  sobrenome.value = ""
+     //  email.value = ""
+     form.reset();
  }
+
+ //  garantizar que los campos estén correctamente preenchidos, "Validación de datos" y/o que no estén vacios al clicar botón
+ function ValidacaoCampo() {
+     if (nome.value == "") {
+         alert("O campo Nome é obrigatório. Favor preenchê-lo.")
+         nome.focus()
+     } else if (sobrenome.value == "") {
+         alert("O campo Sobrenome é obrigatório. Favor preenchê-lo.")
+     } else if (email.value == "") {
+         alert(""
+             O campo Email é obrigatório.Favor preenchê - lo.
+             "")
+     }
+ }
+
  // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
  document.querySelector('#btn_submit').addEventListener('click',
      function (evt) {

049527244b818868d537416a89f523553ffdfb76
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 12:30:43 -0300

    validacion de campo registro nuevo !=0

--- script.js
+++ script.js
@@ -14,43 +14,44 @@
 
  // función para guardar registro, add mensaje de error
  function AddRegistroNovo() {
-     let nomeUsuario = nome.value;
-     let sobrenomeUsuario = sobrenome.value;
-     let emailUsuario = email.value;
-     // Nuevo objeto Usuario para crear conjunto de valores de usuario desde input
-     let usuario = new Usuario(nomeUsuario, sobrenomeUsuario, emailUsuario)
-     //  Vamos a crear una lista de los usuarios a través de este new usuario
-     usuarios.push(usuario);
-     //  limpiar los inputs, no estoy segura...
-     //  nome.value = ""
-     //  sobrenome.value = ""
-     //  email.value = ""
-     form.reset();
- }
-
- //  garantizar que los campos estén correctamente preenchidos, "Validación de datos" y/o que no estén vacios al clicar botón
- function ValidacaoCampo() {
-     if (nome.value == "") {
-         alert("O campo Nome é obrigatório. Favor preenchê-lo.")
-         nome.focus()
-         return 0
-     } else if (sobrenome.value == "") {
-         alert("O campo Sobrenome é obrigatório. Favor preenchê-lo.")
-         sobrenome.focus()
-         return 0
-     } else if (email.value == "") {
-         alert("O campo Email é obrigatório. Favor preenchê-lo.")
-         email.focus()
-         return 0
+     if (ValidacaoCampo() != 0) {
+         let nomeUsuario = nome.value;
+         let sobrenomeUsuario = sobrenome.value;
+         let emailUsuario = email.value;
+         // Nuevo objeto Usuario para crear conjunto de valores de usuario desde input
+         let usuario = new Usuario(nomeUsuario, sobrenomeUsuario, emailUsuario)
+         //  Vamos a crear una lista de los usuarios a través de este new usuario
+         usuarios.push(usuario);
+         //  limpiar los inputs, no estoy segura...
+         //  nome.value = ""
+         //  sobrenome.value = ""
+         //  email.value = ""
+         form.reset();
      }
- }
 
- // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
- document.querySelector('#btn_submit').addEventListener('click',
-     function (evt) {
-         evt.preventDefault();
-     })
- document.querySelector('#btn_submit').addEventListener('click', AddRegistroNovo)
+     //  garantizar que los campos estén correctamente preenchidos, "Validación de datos" y/o que no estén vacios al clicar botón
+     function ValidacaoCampo() {
+         if (nome.value == "") {
+             alert("O campo Nome é obrigatório. Favor preenchê-lo.")
+             nome.focus()
+             return 0
+         } else if (sobrenome.value == "") {
+             alert("O campo Sobrenome é obrigatório. Favor preenchê-lo.")
+             sobrenome.focus()
+             return 0
+         } else if (email.value == "") {
+             alert("O campo Email é obrigatório. Favor preenchê-lo.")
+             email.focus()
+             return 0
+         }
+     }
 
- //  Evento para validación de campo
- document.querySelector('#btn_submit').addEventListener('click', ValidacaoCampo)
+     // linkar la función con el botón y add preventDefault para que la pag no se recargue con el click
+     document.querySelector('#btn_submit').addEventListener('click',
+         function (evt) {
+             evt.preventDefault();
+         })
+     document.querySelector('#btn_submit').addEventListener('click', AddRegistroNovo)
+
+     //  Evento para validación de campo
+     document.querySelector('#btn_submit').addEventListener('click', ValidacaoCampo)

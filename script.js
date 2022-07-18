f9c83c19d1b04e59f3762dc0a37707038e19c944
Gaby Zapata <gabyzda@gmail.com>
2022-07-17 21:38:22 -0300

    email class

--- script.js
+++ script.js
@@ -74,5 +74,5 @@ function disableSubmit() {
 }
 
 document.querySelectorAll('input').forEach(el => el.classList.add ('not-validated'));
-document.querySelectorAll('#input_email').forEach(el => el.addEventListener ('click', validateEmail));
+document.querySelectorAll('input.email').forEach(el => el.addEventListener ('click', validateEmail));
 document.querySelectorAll('input:required').forEach(el => el.addEventListener ('click', validateNotEmpty));

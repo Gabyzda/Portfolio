045d0d9b155fa2e603876c4d81ad6e63b51f1371
Gabriela Zapata <gabyzda@gmail.com>
2022-07-26 00:17:53 -0300

    comillas que faltaban validar email

--- script.js
+++ script.js
@@ -7,52 +7,54 @@ const expresiones = {
     email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
 
-const validarForm = (e) => {
-    switch (e.target.name) {
-        case "nome":
-            if (expresiones.nome.test(e.tatget.value)) {
-                document.querySelector('#campo_nome .form_input-error').classList.remove(form_input-error-active);
-            } else {
-                document.querySelector('#campo_nome .form_input-error').classList.add(form_input-error-active);
-            }
-        break;
+// const validarForm = (e) => { por todos, pero no me gustó :(
+//     switch (e.target.name) {
+//         case "nome":
+//             if (expresiones.nome.test(e.tatget.value)) {
+//                 document.querySelector('#campo_nome .form_input-error').classList.remove(form_input-error-active);
+//             } else {
+//                 document.querySelector('#campo_nome .form_input-error').classList.add(form_input-error-active);
+//             }
+//         break;
 
-        case "sobrenome":
-            if (expresiones.sobrenome.test(e.tatget.value)) {
-                document.querySelector('#campo_sobrenome .form_input-error').classList.remove(form_input-error-active);
-            } else {
-                document.querySelector('#campo_sobrenome .form_input-error').classList.add(form_input-error-active);
-            }
-        break;
+//         case "sobrenome":
+//             if (expresiones.sobrenome.test(e.tatget.value)) {
+//                 document.querySelector('#campo_sobrenome .form_input-error').classList.remove(form_input-error-active);
+//             } else {
+//                 document.querySelector('#campo_sobrenome .form_input-error').classList.add(form_input-error-active);
+//             }
+//         break;
         
+//         case "email":
+//             if (expresiones.email.test(e.tatget.value)) {
+//                 document.querySelector('#campo_email .form_input-error').classList.remove(form_input-error-active);
+//             } else {
+//                 document.querySelector('#campo_email .form_input-error').classList.add(form_input-error-active);
+//             }
+//         break;
+//     }
+// }
+const validarForm = (e) => {
+        switch (e.target.name) {
+        case "nome":
+            validarCampo(expresiones.nome, e.target, 'nome');
+        break;
+        case "sobrenome":
+            validarCampo(expresiones.sobrenome, e.target, 'sobrenome');
+        break;
         case "email":
-            if (expresiones.email.test(e.tatget.value)) {
-                document.querySelector('#campo_email .form_input-error').classList.remove(form_input-error-active);
-            } else {
-                document.querySelector('#campo_email .form_input-error').classList.add(form_input-error-active);
-            }
+            validarCampo(expresiones.email, e.target, 'email');
         break;
     }
 }
-//         case "nome":
-//             validarCampo(expresiones.nome, e.target, 'nome');
-//             break;
-//         case "sobrenome":
-//             validarCampo(expresiones.sobrenome, e.target, 'sobrenome')
-//             break;
-//         case "email":
-//             validarCampo(expresiones.email, e.target, 'email')
-//             break;
-//     }
-// }
 
-// const validarCampo = (expresion, input, campo) => {
-//     if (expresion.test(input.value)) {
-//         document.querySelector(`#campo_${campo} .form_input-error`).classList.remove(form_input-error-active);
-//     } else {
-//         document.querySelector(`#campo_${campo} .form_input-error`).classList.add(form_input-error-active);
-//     }
-// }
+const validarCampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+        document.querySelector(`#campo_${campo} .form_input-error`).classList.remove('form_input-error-active');
+    } else {
+        document.querySelector(`#campo_${campo} .form_input-error`).classList.add('form_input-error-active');
+    }
+}
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarForm);
